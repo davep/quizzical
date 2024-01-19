323be811ce4155c7422a917539d46e9d7da8afb3
Dave Pearson <davep@davep.org>
2024-01-19 15:31:59 +0000

    :card_index: Export some more things from the API

--- quizzical/opentdb/__init__.py
+++ quizzical/opentdb/__init__.py
@@ -4,9 +4,28 @@
 # Local imports.
 from .category import Category
 from .client import OpenTriviaDB
+from .question import Question
+from .response import (
+    Code,
+    InvalidParameter,
+    NoResults,
+    RateLimit,
+    TokenEmpty,
+    TokenNotFound,
+)
 
 ##############################################################################
 # Exports.
-__all__ = ["Category", "OpenTriviaDB"]
+__all__ = [
+    "Category",
+    "Code",
+    "InvalidParameter",
+    "NoResults",
+    "OpenTriviaDB",
+    "Question",
+    "RateLimit",
+    "TokenEmpty",
+    "TokenNotFound",
+]
 
 ### __init__.py ends here
