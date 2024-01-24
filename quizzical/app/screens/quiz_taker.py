f141327ece8d0600d2b46534a906c748e1b369d1
Dave Pearson <davep@davep.org>
2024-01-24 09:13:01 +0000

    :construction: More tinkering with the quiz taker dialog

--- quizzical/app/screens/quiz_taker.py
+++ quizzical/app/screens/quiz_taker.py
@@ -6,7 +6,7 @@ from textual import work
 from textual.app import ComposeResult
 from textual.containers import Center, Vertical
 from textual.screen import ModalScreen
-from textual.widgets import Button, LoadingIndicator, Pretty
+from textual.widgets import Button, Label, LoadingIndicator
 
 ##############################################################################
 # Local imports.
@@ -54,7 +54,7 @@ class QuizTaker(ModalScreen):
         """The trivia database client object."""
         self._quiz_parameters = quiz
         """The parameters of the quiz we're going to take."""
-        self._quiz = list[Question]
+        self._quiz: list[Question] = []
 
     def compose(self) -> ComposeResult:
         """Compose the quiz taking screen."""
@@ -65,15 +65,44 @@ class QuizTaker(ModalScreen):
             with Center():
                 yield Button("Cancel")  # TODO
 
+        with Vertical(id="confirmer", classes="hidden") as quiz_confirmer:
+            quiz_confirmer.border_title = (
+                f"Ready to take '{self._quiz_parameters.title}'?"
+            )
+            yield Label("The quiz is now loaded and ready; are you all set to take it?")
+            yield Label(id="details")
+            with Center():
+                yield Button("Let's go!", id="take")
+                yield Button("Cancel", id="cancel")
+
         with Vertical(id="taker", classes="hidden") as quiz_taker:
             quiz_taker.border_title = self._quiz_parameters.title
-            yield Pretty([])
             with Center():
                 yield Button("Worked")
 
     def on_mount(self) -> None:
         """Start the process of loading up the quiz once the DOM is ready."""
         self._load_quiz()
+
+    @property
+    def _quiz_summary(self) -> str:
+        """A descriptive summary of the quiz."""
+        summary = f"{len(self._quiz)} question{'' if len(self._quiz) == 1 else 's'}\n"
+        if self._quiz_parameters.category is None:
+            summary += "Any category.\n"
+        else:
+            summary += f"{self._quiz_parameters.category.name}\n"
+        if self._quiz_parameters.difficulty is None:
+            summary += "Any difficulty level.\n"
+        else:
+            summary += (
+                f"{self._quiz_parameters.difficulty.capitalize()} difficulty level.\n"
+            )
+        if self._quiz_parameters.question_type is None:
+            summary += "Multiple choice and true/false questions."
+        else:
+            summary += f"{'True or false' if self._quiz_parameters.question_type == 'boolean' else 'Multiple choice'} questions."
+        return summary
 
     @work
     async def _load_quiz(self) -> None:
@@ -86,8 +115,9 @@ class QuizTaker(ModalScreen):
         )
         with self.app.batch_update():
             self.query_one("#loader").set_class(True, "hidden")
-            self.query_one("#taker").set_class(False, "hidden")
-            self.query_one(Pretty).update(self._quiz)
+            self.query_one("#confirmer").set_class(False, "hidden")
+            self.query_one("#taker").set_class(True, "hidden")
+            self.query_one("#confirmer #details", Label).update(self._quiz_summary)
 
 
 ### quiz_taker.py ends here
