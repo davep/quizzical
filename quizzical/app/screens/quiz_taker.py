54bf998a068e5c81ce5e8e173f380465ff6db0b6
Dave Pearson <davep@davep.org>
2024-01-24 21:51:45 +0000

    :construction: More work in progress on the quiz taking dialog

--- quizzical/app/screens/quiz_taker.py
+++ quizzical/app/screens/quiz_taker.py
@@ -4,9 +4,11 @@
 # Textual imports.
 from textual import on, work
 from textual.app import ComposeResult
-from textual.containers import Center, Vertical
+from textual.containers import Center, Grid, Vertical
+from textual.css.query import NoMatches
+from textual.reactive import var
 from textual.screen import ModalScreen
-from textual.widgets import Button, Label, LoadingIndicator
+from textual.widgets import Button, Digits, Label, LoadingIndicator, RadioSet
 
 ##############################################################################
 # Local imports.
@@ -60,11 +62,51 @@ class QuizTaker(ModalScreen):
         }
 
         #taker {
-            width: 80%;
-            height: 80%;
+            width: auto;
+            max-width: 60;
+            height: auto;
+
+            Grid {
+                grid-size: 4 2;
+                height: auto;
+                grid-rows: auto;
+                Label, Digits {
+                    width: 1fr;
+                    text-align: center;
+                }
+            }
+
+            #question-text {
+                width: 1fr;
+                height: auto;
+                padding: 2;
+                text-style: bold;
+            }
+
+            #answers {
+                width: 1fr;
+                border: none;
+                margin: 0 1 1 1;
+                padding: 1;
+                &:focus {
+                    border: none;
+                }
+            }
         }
     }
     """
+
+    _question: var[int] = var(0, init=False)
+    """The number of the question being asked."""
+
+    _answers: var[list[str]] = var(list, init=False)
+    """Holds the answers given."""
+
+    _correct: var[int] = var(0, init=False)
+    """The count of correct answers."""
+
+    _wrong: var[int] = var(0, init=False)
+    """The count of wrong answers."""
 
     def __init__(self, client: OpenTriviaDB, quiz: QuizParameters) -> None:
         """Initialise the quiz taking screen.
@@ -87,7 +129,7 @@ class QuizTaker(ModalScreen):
             quiz_loader.border_title = f"Loading: {self._quiz_parameters.title}"
             yield LoadingIndicator()
             with Center():
-                yield Button("Cancel")  # TODO
+                yield Button("Cancel", id="cancel")
 
         with Vertical(id="confirmer", classes="hidden") as quiz_confirmer:
             quiz_confirmer.border_title = (
@@ -102,8 +144,18 @@ class QuizTaker(ModalScreen):
 
         with Vertical(id="taker", classes="hidden") as quiz_taker:
             quiz_taker.border_title = self._quiz_parameters.title
+            with Grid():
+                yield Label("Question")
+                yield Label("Out Of")
+                yield Label("Correct")
+                yield Label("Wrong")
+                yield Digits("0", id="question")
+                yield Digits("0", id="out-of")
+                yield Digits("0", id="correct")
+                yield Digits("0", id="wrong")
+            yield Label(id="question-text")
             with Center():
-                yield Button("Worked")
+                yield Button("Cancel", id="cancel")
 
     def on_mount(self) -> None:
         """Start the process of loading up the quiz once the DOM is ready."""
@@ -125,7 +177,7 @@ class QuizTaker(ModalScreen):
         if self._quiz_parameters.question_type is None:
             summary += "Multiple choice and true/false questions\n"
         else:
-            summary += f"{'True or false' if self._quiz_parameters.question_type == 'boolean' else 'Multiple choice'} questions."
+            summary += f"{'True or false' if self._quiz_parameters.question_type == 'boolean' else 'Multiple choice'} questions\n"
         if self._quiz_parameters.timer_type == QuizTimer.PER_QUESTION:
             summary += f"{self._quiz_parameters.timer_value} seconds per question\n"
         if self._quiz_parameters.timer_type == QuizTimer.WHOLE_QUIZ:
@@ -152,10 +204,52 @@ class QuizTaker(ModalScreen):
             )
             self.query_one("#take").focus()
 
+    async def _watch__question(self) -> None:
+        self.query_one("#question", Digits).update(str(self._question))
+        self.query_one("#question-text", Label).update(
+            self._quiz[self._question - 1].question
+        )
+        try:
+            await self.query_one("#answers").remove()
+        except NoMatches:
+            pass
+        await self.query_one("#taker").mount(
+            RadioSet(
+                *[answer for answer in self._quiz[self._question - 1].answers],
+                id="answers",
+            ),
+            after="#question-text",
+        )
+
+    def _watch__correct(self) -> None:
+        self.query_one("#correct", Digits).update(str(self._correct))
+
+    def _watch__wrong(self) -> None:
+        self.query_one("#wrong", Digits).update(str(self._wrong))
+
     @on(Button.Pressed, "#cancel")
     def cancel_quiz(self) -> None:
         """Cancel the current quiz."""
         self.dismiss()
+
+    @on(Button.Pressed, "#take")
+    def take_quiz(self) -> None:
+        with self.app.batch_update():
+            self.query_one("#loader").set_class(True, "hidden")
+            self.query_one("#confirmer").set_class(True, "hidden")
+            self.query_one("#taker").set_class(False, "hidden")
+            self.query_one("#out-of", Digits).update(str(len(self._quiz)))
+            self._question = 1
+
+    @on(RadioSet.Changed)
+    def answered(self, event: RadioSet.Changed) -> None:
+        question = self._quiz[self._question - 1]
+        if question.correct_answer == question.answers[event.index]:
+            self._correct += 1
+        else:
+            self._wrong += 1
+        self._answers.append(self._quiz[self._question - 1].answers[event.index])
+        self._question += 1
 
 
 ### quiz_taker.py ends here
