6d498cc2e7fafecb880e4a8a799b38e3f725a89c
Dave Pearson <davep@davep.org>
2024-01-24 20:29:27 +0000

    :construction: Move work on the quiz-taking dialog

--- quizzical/app/screens/quiz_taker.py
+++ quizzical/app/screens/quiz_taker.py
@@ -2,7 +2,7 @@
 
 ##############################################################################
 # Textual imports.
-from textual import work
+from textual import on, work
 from textual.app import ComposeResult
 from textual.containers import Center, Vertical
 from textual.screen import ModalScreen
@@ -11,7 +11,7 @@ from textual.widgets import Button, Label, LoadingIndicator
 ##############################################################################
 # Local imports.
 from ...opentdb import OpenTriviaDB, Question
-from ..data.quiz_parameters import QuizParameters
+from ..data.quiz_parameters import QuizParameters, QuizTimer
 
 
 ##############################################################################
@@ -38,6 +38,24 @@ class QuizTaker(ModalScreen):
             height: auto;
             LoadingIndicator {
                height: 3;
+            }
+        }
+
+        #confirmer {
+            padding: 1 1 0 1;
+            width: auto;
+            height: auto;
+            Label {
+                width: auto;
+                margin-bottom: 1;
+                text-align: center;
+            }
+            Center {
+                width: 100%;
+                layout: horizontal;
+            }
+            Button {
+                margin-right: 1;
             }
         }
 
@@ -70,10 +88,11 @@ class QuizTaker(ModalScreen):
                 f"Ready to take '{self._quiz_parameters.title}'?"
             )
             yield Label("The quiz is now loaded and ready; are you all set to take it?")
-            yield Label(id="details")
             with Center():
-                yield Button("Let's go!", id="take")
-                yield Button("Cancel", id="cancel")
+                yield Label(id="details")
+            with Center():
+                yield Button("Let's go!", id="take", variant="success")
+                yield Button("Cancel", id="cancel", variant="error")
 
         with Vertical(id="taker", classes="hidden") as quiz_taker:
             quiz_taker.border_title = self._quiz_parameters.title
@@ -88,19 +107,25 @@ class QuizTaker(ModalScreen):
         """A descriptive summary of the quiz."""
         summary = f"{len(self._quiz)} question{'' if len(self._quiz) == 1 else 's'}\n"
         if self._quiz_parameters.category is None:
-            summary += "Any category.\n"
+            summary += "Any category\n"
         else:
             summary += f"{(await self._client.category(self._quiz_parameters.category)).name}\n"
         if self._quiz_parameters.difficulty is None:
-            summary += "Any difficulty level.\n"
+            summary += "Any difficulty level\n"
         else:
             summary += (
-                f"{self._quiz_parameters.difficulty.capitalize()} difficulty level.\n"
+                f"{self._quiz_parameters.difficulty.capitalize()} difficulty level\n"
             )
         if self._quiz_parameters.question_type is None:
-            summary += "Multiple choice and true/false questions."
+            summary += "Multiple choice and true/false questions\n"
         else:
             summary += f"{'True or false' if self._quiz_parameters.question_type == 'boolean' else 'Multiple choice'} questions."
+        if self._quiz_parameters.timer_type == QuizTimer.PER_QUESTION:
+            summary += f"{self._quiz_parameters.timer_value} seconds per question\n"
+        if self._quiz_parameters.timer_type == QuizTimer.WHOLE_QUIZ:
+            summary += (
+                f"{self._quiz_parameters.timer_value} seconds to complete the quiz\n"
+            )
         return summary
 
     @work
@@ -119,6 +144,12 @@ class QuizTaker(ModalScreen):
             self.query_one("#confirmer #details", Label).update(
                 await self._quiz_summary()
             )
+            self.query_one("#take").focus()
+
+    @on(Button.Pressed, "#cancel")
+    def cancel_quiz(self) -> None:
+        """Cancel the current quiz."""
+        self.dismiss()
 
 
 ### quiz_taker.py ends here
