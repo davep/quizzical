630e4badffec59852eba9e155055b99b7b640e96
Dave Pearson <davep@davep.org>
2024-01-29 19:51:19 +0000

    :lipstick: Have the logo hold up to being made quite narrow

--- quizzical/app/widgets/logo.py
+++ quizzical/app/widgets/logo.py
@@ -1,17 +1,25 @@
 """Provides a logo widget."""
 
 ##############################################################################
+# Python imports.
+from typing import Final
+
+##############################################################################
 # Textual imports.
 from textual.app import RenderResult
 from textual.geometry import Size
 from textual.widget import Widget
 
 ##############################################################################
-LOGO = r""" ____   ____   ____   ____   ____   ____   ____   ____   ____
+LOGO: Final[
+    str
+] = r""" ____   ____   ____   ____   ____   ____   ____   ____   ____
 ||Q || ||u || ||i || ||z || ||z || ||i || ||c || ||a || ||l ||
 ||__|| ||__|| ||__|| ||__|| ||__|| ||__|| ||__|| ||__|| ||__||
 |/__\| |/__\| |/__\| |/__\| |/__\| |/__\| |/__\| |/__\| |/__\|
 """
+LOGO_MEDIUM: Final[str] = "\n\nQ u i z z i c a l"
+LOGO_SMALL: Final[str] = "\n\nQuizzical"
 
 
 ##############################################################################
@@ -31,6 +39,10 @@ class Logo(Widget):
 
     def render(self) -> RenderResult:
         """Render the logo."""
+        if self.container_size.width < 20:
+            return LOGO_SMALL
+        elif self.container_size.width < 65:
+            return LOGO_MEDIUM
         return LOGO
 
     def get_content_height(self, container: Size, viewport: Size, width: int) -> int:
