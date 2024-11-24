9c64a1108469f1da70af590384c5070902d047bc
Dave Pearson <davep@davep.org>
2024-11-24 14:30:48 +0000

    :card_index: Set __version__ from the package data

--- src/quizzical/__init__.py
+++ src/quizzical/__init__.py
@@ -1,5 +1,9 @@
 """Application information."""
 
+##############################################################################
+# Python imports.
+from importlib.metadata import version
+
 ######################################################################
 # Main app information.
 __author__ = "Dave Pearson"
@@ -7,7 +11,7 @@ __copyright__ = "Copyright 2024, Dave Pearson"
 __credits__ = ["Dave Pearson"]
 __maintainer__ = "Dave Pearson"
 __email__ = "davep@davep.org"
-__version__ = "0.1.1"
+__version__ = version("quizzical")
 __licence__ = "GPLv3+"
 
 ##############################################################################
